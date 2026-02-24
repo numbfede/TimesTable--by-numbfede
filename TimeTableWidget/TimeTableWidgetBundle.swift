a6809c4a25c4b0d5ccd331eec47637ac5e63a64f
Federico <federico.musso@supsi.ch>
2026-02-24 15:19:02 +0100

    final commit?

---
+++ TimeTableWidget/TimeTableWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTableWidgetBundle.swift
+//  TimeTableWidget
+//
+//  Created by Federico Musso on 24/02/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TimeTableWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TimeTableWidget()
+        TimeTableWidgetControl()
+    }
+}
